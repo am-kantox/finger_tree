a08374979514b07fa78dcb182b664c90118e863e
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2020-02-12 10:37:00 +0100

    Test for huge tree

--- test/finger_tree_test.exs
+++ test/finger_tree_test.exs
@@ -3,51 +3,69 @@ defmodule FingerTreeTest do
   doctest FingerTree
 
   @empty %FingerTree.Empty{contents: []}
+  @range ?a..?z
+  @inversed_range @range.last..@range.first
+  @double_range @range.first..(2 * @range.last - @range.first + 1)
 
   setup_all do
     %{
       tree_fwd_push:
-        Enum.reduce(?a..?z, %FingerTree.Empty{}, fn x, acc -> FingerTree.push(acc, x) end),
+        Enum.reduce(@range, %FingerTree.Empty{}, fn x, acc -> FingerTree.push(acc, x) end),
       tree_bwd_push:
-        Enum.reduce(?z..?a, %FingerTree.Empty{}, fn x, acc -> FingerTree.push(acc, x) end),
+        Enum.reduce(@inversed_range, %FingerTree.Empty{}, fn x, acc -> FingerTree.push(acc, x) end),
       tree_fwd_unsh:
-        Enum.reduce(?a..?z, %FingerTree.Empty{}, fn x, acc -> FingerTree.unshift(acc, x) end),
+        Enum.reduce(@range, %FingerTree.Empty{}, fn x, acc -> FingerTree.unshift(acc, x) end),
       tree_bwd_unsh:
-        Enum.reduce(?z..?a, %FingerTree.Empty{}, fn x, acc -> FingerTree.unshift(acc, x) end)
+        Enum.reduce(@inversed_range, %FingerTree.Empty{}, fn x, acc ->
+          FingerTree.unshift(acc, x)
+        end)
     }
   end
 
-  test "push/2 + pop/1", ctx do
-    assert Enum.map_reduce(?a..?z, ctx.tree_fwd_push, fn _, acc ->
+  test "huge tree → push/2 + pop/1" do
+    huge_range = ?a..(10_000 + ?a)
+
+    tree_huge =
+      Enum.reduce(huge_range, %FingerTree.Empty{}, fn x, acc ->
+        FingerTree.push(acc, x)
+      end)
+
+    assert Enum.map_reduce(huge_range, tree_huge, fn _, acc ->
              {FingerTree.first(acc), FingerTree.pop(acc)}
-           end) == {Enum.to_list(?z..?a), @empty}
+           end) == {Enum.to_list(huge_range.last..huge_range.first), @empty}
+  end
+
+  test "push/2 + pop/1", ctx do
+    assert Enum.map_reduce(@range, ctx.tree_fwd_push, fn _, acc ->
+             {FingerTree.first(acc), FingerTree.pop(acc)}
+           end) == {Enum.to_list(@inversed_range), @empty}
   end
 
   test "push/2 + shift/1", ctx do
-    assert Enum.map_reduce(?a..?z, ctx.tree_fwd_push, fn _, acc ->
+    assert Enum.map_reduce(@range, ctx.tree_fwd_push, fn _, acc ->
              {FingerTree.last(acc), FingerTree.shift(acc)}
-           end) == {Enum.to_list(?a..?z), @empty}
+           end) == {Enum.to_list(@range), @empty}
   end
 
   test "unshift/2 + pop/1", ctx do
-    assert Enum.map_reduce(?a..?z, ctx.tree_fwd_unsh, fn _, acc ->
+    assert Enum.map_reduce(@range, ctx.tree_fwd_unsh, fn _, acc ->
              {FingerTree.first(acc), FingerTree.pop(acc)}
-           end) == {Enum.to_list(?a..?z), @empty}
+           end) == {Enum.to_list(@range), @empty}
   end
 
   test "unshift/2 + shift/1", ctx do
-    assert Enum.map_reduce(?a..?z, ctx.tree_fwd_unsh, fn _, acc ->
+    assert Enum.map_reduce(@range, ctx.tree_fwd_unsh, fn _, acc ->
              {FingerTree.last(acc), FingerTree.shift(acc)}
-           end) == {Enum.to_list(?z..?a), @empty}
+           end) == {Enum.to_list(@inversed_range), @empty}
   end
 
   test "append/2", ctx do
     double = FingerTree.append(ctx.tree_fwd_push, ctx.tree_fwd_push)
 
-    assert Enum.map_reduce(?a..(?z + 26), double, fn _, acc ->
+    assert Enum.map_reduce(@double_range, double, fn _, acc ->
              {FingerTree.last(acc), FingerTree.shift(acc)}
            end) ==
-             {?a..?z |> Enum.to_list() |> List.duplicate(2) |> Enum.reduce(&Kernel.++/2), @empty}
+             {@range |> Enum.to_list() |> List.duplicate(2) |> Enum.reduce(&Kernel.++/2), @empty}
   end
 
   test "prepend/2", ctx do
@@ -57,12 +75,12 @@ defmodule FingerTreeTest do
     assert a == p
 
     {'abcdefghijklmnopqrstuvwxyz', bwd} =
-      Enum.map_reduce(?a..?z, a, fn _, acc ->
+      Enum.map_reduce(@range, a, fn _, acc ->
         {FingerTree.first(acc), FingerTree.pop(acc)}
       end)
 
     assert {'abcdefghijklmnopqrstuvwxyz', @empty} =
-             Enum.map_reduce(?a..?z, bwd, fn _, acc ->
+             Enum.map_reduce(@range, bwd, fn _, acc ->
                {FingerTree.last(acc), FingerTree.shift(acc)}
              end)
   end
